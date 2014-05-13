21e6efe69a9a3df89be3d84232d4f5196c356d7c
Martin Lenders <mail@martin-lenders.de>
2014-05-13 09:41:55 +0200

    Create Board model

--- board_app_creator/models.py
+++ board_app_creator/models.py
@@ -1,3 +1,22 @@
+"""
+Models for board_app_creator application.
+"""
 from django.db import models
+import board_app_creator.fields as fields
 
-# Create your models here.
+class Board(models.Model):
+    """
+    A board in the RIOT repository.
+    """
+    riot_name = models.CharField(max_length=16, unique=True, blank=False,
+                                 null=False)
+    repository = models.URLField(blank=False, null=False)
+    repository_tree = models.CharField(max_length=32, blank=False, null=False)
+    cpu_repository = models.URLField()
+    cpu_repository_tree = models.CharField(max_length=32, blank=False,
+                                           null=False)
+    usb_vid = fields.SmallHexField()
+    usb_pid = fields.SmallHexField()
+
+    def __str__(self):
+        return self.riot_name
