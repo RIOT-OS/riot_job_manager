1492a7251a88774bacecf1c62882d9668952a152
Martin Lenders <mail@martin-lenders.de>
2014-06-02 08:35:43 +0200

    Rename prototype_job in job creation by prototype

--- board_app_creator/forms.py
+++ board_app_creator/forms.py
@@ -60,7 +60,7 @@ class BoardForm(forms.ModelForm):
             ).values_list('pk', 'name')
 
 class JobFromPrototypeForm(forms.ModelForm):
-    prototype = forms.ModelChoiceField(queryset=models.ApplicationJob.objects.all())
+    prototype_job = forms.ModelChoiceField(queryset=models.ApplicationJob.objects.all())
     next_application = forms.CharField(widget=forms.HiddenInput())
     next_board = forms.CharField(widget=forms.HiddenInput())
 
